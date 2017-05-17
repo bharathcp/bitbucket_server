8fec761cbf51ad52b73e6e4d2b696e75e0653803
Raghu <graghav@gmail.com>
2017-05-17 16:45:53 +1200

    Perl profile added in InSpec test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -14,13 +14,21 @@ describe command('java -version') do
   its('stderr') { should be > 1.8 }
 end
 
-describe command('which perl ') do
-  it { should exist }
-  its('exit_status') { should eq 0 }
-end
-
-describe command('perl -e "print $]"') do
-  its('stdout') { should be > 5.010 }
+control 'check-perl' do
+  title 'Check perl compiled options and version'
+  perl_out = command('perl -V')
+  perl_ver = command('perl -e "print $]"')
+  describe perl_out do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match (/USE_64_BIT_ALL/) }
+    its('stdout') { should match (/useposix=true/) }
+    its('stdout') { should match (/-fstack-protector/) }
+  end
+  get_perl_ver = perl_ver.stdout.to_f
+  describe get_perl_ver do
+    it { should be > 5.010 }
+  end 
+    
 end
 
 describe command('which git ') do
