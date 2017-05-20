6a0be5bae3652d4999f65f24cc83cbac69188a86
Raghu <graghav@gmail.com>
2017-05-20 21:07:43 +1200

    Refactored Bitbucket Install steps as Custom Resource

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name 'bitbucket_server'
-maintainer 'The Authors'
+maintainer 'Bharath, Raghavendra Gona'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures bitbucket_server'
 long_description 'Installs/Configures bitbucket_server'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.4' if respond_to?(:chef_version)
 
-depends 'java', '~> 1.49.0'
-depends 'git', '~> 6.0.0'
+#depends 'java', '~> 1.49.0'
+#depends 'git', '~> 6.0.0'
 depends 'ark', '~> 3.1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe 'bitbucket_server::linux_standalone'
-include_recipe 'bitbucket_server::configuration'
-include_recipe 'bitbucket_server::service_init'
+bitbucket_server_install 'altbitbucket' do
+  bitbucket_user 'altbit'
+  bitbucket_group 'altbit'
+end
+
+#include_recipe 'bitbucket_server::linux_standalone'
+#include_recipe 'bitbucket_server::configuration'
+#include_recipe 'bitbucket_server::service_init'
