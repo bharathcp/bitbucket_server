455c578bcbcef69ca4fb849ae6fae81f1be95893
Bharath Prakash <bharath.prakash@assurity.co.nz>
2017-05-24 08:43:51 +1200

    deleting the unnecessary recipe

--- recipes/install.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook:: bitbucket_server
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-bitbucket_install 'bitbucket' do
-  jre_home "#{node['java']['java_home']}/jre"
-end
