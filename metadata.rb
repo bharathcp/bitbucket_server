5e0e39d247be1d89ee9dcbce96b502dd72f8c326
Raghu <graghav@gmail.com>
2017-05-23 15:48:29 +1200

    adding java and git depends

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description 'Installs/Configures bitbucket_server'
 version '0.1.0'
 chef_version '>= 12.4' if respond_to?(:chef_version)
 
-#depends 'java', '~> 1.49.0'
-#depends 'git', '~> 6.0.0'
+depends 'java', '~> 1.49.0'
+depends 'git', '~> 6.0.0'
 depends 'ark', '~> 3.1.0'
