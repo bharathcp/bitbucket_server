de5bc826272cfeb1f363642f62ec9e1868440ef1
Raghu <graghav@gmail.com>
2017-05-24 15:07:04 +1200

    centos version to 7.3

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'cippy.bharath@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures bitbucket_server'
 long_description 'Installs/Configures bitbucket_server'
-version '0.1.2'
+version '0.1.3'
 chef_version '>= 12.4' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/bharathcp/bitbucket_server/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/bharathcp/bitbucket_server' if respond_to?(:source_url)
 
-supports 'centos', '>= 7'
+supports 'centos', '>= 7.3'
 
 depends 'java', '~> 1.49.0'
 depends 'git', '~> 6.0.0'
