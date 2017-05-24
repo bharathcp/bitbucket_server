e09fe4ea7a0dcb14bf6928bd5e690fbff25d4bf1
Raghu <graghav@gmail.com>
2017-05-24 14:54:59 +1200

    release ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cippy.bharath@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures bitbucket_server'
 long_description 'Installs/Configures bitbucket_server'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.4' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/bharathcp/bitbucket_server/issues' if respond_to?(:issues_url)
