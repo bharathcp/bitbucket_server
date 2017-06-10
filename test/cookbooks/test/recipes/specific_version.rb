2fcccbe06c09a80c6ee7e1be97a0db1f4f2b1fb6
Bharath Prakash <bharath.prakash@assurity.co.nz>
2017-06-10 23:07:26 +1200

    fixes #34 #32 #10

---
+++ test/cookbooks/test/recipes/specific_version.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: bitbucket_server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+bitbucket_install 'bitbucket' do
+  jre_home "#{node['java']['java_home']}/jre"
+  jvm_args node['bitbucket']['jvm_args']
+  version node['bitbucket']['version']
+end
+
+bitbucket_config 'bitbucket' do
+  bitbucket_properties node['bitbucket']['properties']
+end
+
+bitbucket_service 'bitbucket'
